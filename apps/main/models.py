515c1e941e3516fdbb865832c901bb4d72a50720
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-02-06 21:49:49 +0500

    Initial

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.db import models as db
 
 
+class Visitor(db.Model):
+    ip = db.CharField(max_length=100, verbose_name='Ip')
+
+    class Meta:
+        verbose_name = 'Tags'
+        verbose_name_plural = verbose_name
--- apps/main/views.py
+++ apps/main/views.py
@@ -9,22 +9,23 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from blog.models import Post, Category, Tag
+from main.models import Visitor
 
 
-# def get_client_ip(request):
-#     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-#     if x_forwarded_for:
-#         ip = x_forwarded_for.split(',')[0]
-#     else:
-#         ip = request.META.get('REMOTE_ADDR')
-#     return print(ip)
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return Visitor(ip=ip).save()
 
 
 class MainView(View):
     """Основная страница"""
     def get(self, request):
 
-        # get_client_ip(request)
+        get_client_ip(request)
 
     	# Филтруеть посты если пост не черновык то выведёт пост
         query = Post.objects.filter(draft=False).order_by('-date')
