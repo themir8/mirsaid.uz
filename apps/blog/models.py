d62b20530be85ede3902192127939aa2ef13d4fe
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-02-06 22:25:23 +0500

    Initial

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -54,7 +54,7 @@ class Post(db.Model):
     )
     tag = db.ManyToManyField(Tag, verbose_name='Tags')
 
-    ip = db.CharField(max_length=100, verbose_name='Ip')
+    ip = db.CharField(max_length=100, verbose_name='Ip', null=True)
 
     date = db.DateTimeField("Дата", default=timezone.now)
 
