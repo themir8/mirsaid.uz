7c3a12655f7d3837c73539a4226791491fbc4539
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-02-13 17:11:46 +0500

    Initial

--- siteengine/settings.py
+++ siteengine/settings.py
@@ -114,7 +114,7 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
