86ae29c2339163c0395bd0a88f4fea2aa5c82782
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-02-13 16:43:34 +0500

    Initial

--- siteengine/settings.py
+++ siteengine/settings.py
@@ -60,7 +60,10 @@ WSGI_APPLICATION = 'siteengine.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }    
 }
 
 
